3b9e48b0b137a1594a01b614e81bd1ebfb46cebe
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-02-10 22:28:24 +0000

    js attempts

--- main.js
+++ main.js
@@ -1,22 +1,50 @@
-let level1PiecesUsed = 0;
-let level2PiecesUsed = 0;
-let level3PiecesUsed = 0;
-let level4PiecesUsed = 0;
+// let level1PiecesUsed = 0;
+// let level2PiecesUsed = 0;
+// let level3PiecesUsed = 0;
+// let level4PiecesUsed = 0;
 
 
 
-const printLevel1Pieces = () => {
-    return (24 - level1PiecesUsed);
-};
+// const calcRemainingPieces = () => {
 
-const printLevel2Pieces = () => {
-    return (18 - level2PiecesUsed);
-};
+//         let level1Remaining = (22 - level1PiecesUsed).innerText;
+//         let level1Remaining = document.getElementById('level-1-remaining');
 
-const printLevel3Pieces = () => {
-    return (14 - level3PiecesUsed);
-};
+//         let level2Remaining = (18 - level2PiecesUsed).innerText;
+//         let level2Remaining = document.getElementById('level-2-remaining');
 
-const printLevel4Pieces = () => {
-    return (18 - level4PiecesUsed);
-};
+//         let level3Remaining = (14 - level3PiecesUsed).innerText;
+//         let level3Remaining = document.getElementById('level-3-remaining');
+
+//         let level4Remaining = (18 - level4PiecesUsed).innerText;
+//         let level4Remaining = document.getElementById('level-4-remaining');
+// };
+
+
+const logOnCosole = () => {
+   return console.log(cats r grate)
+}
+
+
+
+// const calcRemainingPieces = () => {
+
+//     const printLevel1Pieces = () => {
+//         return (24 - level1PiecesUsed).innerText = document.getElementById('level-1-remaining');
+//     };
+
+//     const printLevel2Pieces = () => {
+//         return (18 - level2PiecesUsed).innerText = document.getElementById('level-2-remaining');
+//     };
+
+//     const printLevel3Pieces = () => {
+//         return (14 - level3PiecesUsed).innerText = document.getElementById('level-3-remaining');
+//     };
+
+//     const printLevel4Pieces = () => {
+//         return (18 - level4PiecesUsed).innerText = document.getElementById('level-4-remaining');
+//     };
+
+// return
+
+// }
