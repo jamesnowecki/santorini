c260f401ac1d9f5188306634a8da764c97890966
JAMES NOWECKI <JAMESNOWECKI@gmail.com>
2020-02-10 21:29:31 +0000

    basic html css and js done

---
+++ main.js
@@ -0,0 +1,22 @@
+let level1PiecesUsed = 0;
+let level2PiecesUsed = 0;
+let level3PiecesUsed = 0;
+let level4PiecesUsed = 0;
+
+
+
+const printLevel1Pieces = () => {
+    return (24 - level1PiecesUsed);
+};
+
+const printLevel2Pieces = () => {
+    return (18 - level2PiecesUsed);
+};
+
+const printLevel3Pieces = () => {
+    return (14 - level3PiecesUsed);
+};
+
+const printLevel4Pieces = () => {
+    return (18 - level4PiecesUsed);
+};
