8a060afd935a2259c911c2feb1bcdca6d2ea5ac9
James Nowecki <James.Nowecki@Nology.local>
2020-02-25 15:59:03 +0000

    height increase functions work

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 //define the grid objects
 
 class GridSquare {
-   constructor(height, occupant, adjacencies) {
+   constructor(position, height, occupant, adjacencies) {
+      this.position = position;
       this.height = height;
       this.occupant = occupant;
       this.adjacencies = adjacencies;
@@ -12,36 +13,38 @@ class GridSquare {
    }
 }
 
-const a1 = new GridSquare(0, "empty", ["a2", "b1", "b2"]);
-const a2 = new GridSquare(0, "empty", ["a1", "a3", "b1", "b2", "b3"]);
-const a3 = new GridSquare(0, "empty", ["a2", "a4", "b2", "b3", "b4"]);
-const a4 = new GridSquare(0, "empty", ["a3", "a5", "b3", "b4", "b5"]);
-const a5 = new GridSquare(0, "empty", ["a4", "b4", "b5"]);
-const b1 = new GridSquare(0, "empty", ["a1", "a2", "b2", "c1", "c2"]);
-const b2 = new GridSquare(0, "empty", ["a1", "a2", "a3", "b1", "b3", "c1", "c2", "c3"]);
-const b3 = new GridSquare(0, "empty", ["a2", "a3", "a4", "b2", "b4", "c2", "c3", "c4"]);
-const b4 = new GridSquare(0, "empty", ["a3", "a4", "a5", "b3", "b5", "c3", "c4", "c5"]);
-const b5 = new GridSquare(0, "empty", ["a4", "a5", "b4", "c4", "c5"]);
-const c1 = new GridSquare(0, "empty", ["b1", "b2", "c2", "d1", "d2"]);
-const c2 = new GridSquare(0, "empty", ["b1", "b2", "b3", "c1", "c3", "d1", "d2", "d3"]);
-const c3 = new GridSquare(0, "empty", ["b2", "b3", "b4", "c2", "c4", "d2", "d3", "d4"]);
-const c4 = new GridSquare(0, "empty", ["b3", "b4", "b5", "c3", "c5", "d3", "d4", "d5"]);
-const c5 = new GridSquare(0, "empty", ["b4", "b5", "c4", "d4", "d5"]);
-const d1 = new GridSquare(0, "empty", ["c1", "c2", "d2", "e1", "e2"]);
-const d2 = new GridSquare(0, "empty", ["c1", "c2", "c3", "d1", "d3", "e1", "e2", "e3"]);
-const d3 = new GridSquare(0, "empty", ["c2", "c3", "c4", "d2", "d4", "e2", "e3", "e4"]);
-const d4 = new GridSquare(0, "empty", ["c3", "c4", "c5", "d3", "d5", "e3", "e4", "e5"]);
-const d5 = new GridSquare(0, "empty", ["c4", "c5", "d4", "e4", "e5"]);
-const e1 = new GridSquare(0, "empty", ["d1", "d2", "e2"]);
-const e2 = new GridSquare(0, "empty", ["d1", "d2", "d3", "e1", "e3"]);
-const e3 = new GridSquare(0, "empty", ["d2", "d3", "d4", "e2", "e4"]);
-const e4 = new GridSquare(0, "empty", ["d3", "d4", "d5", "e3", "e5"]);
-const e5 = new GridSquare(0, "empty", ["d4", "d5", "e4"]);
+const a1 = new GridSquare("a1", 0, "empty", ["a2", "b1", "b2"]);
+const a2 = new GridSquare("a2", 0, "empty", ["a1", "a3", "b1", "b2", "b3"]);
+const a3 = new GridSquare("a3", 0, "empty", ["a2", "a4", "b2", "b3", "b4"]);
+const a4 = new GridSquare("a4", 0, "empty", ["a3", "a5", "b3", "b4", "b5"]);
+const a5 = new GridSquare("a5", 0, "empty", ["a4", "b4", "b5"]);
+const b1 = new GridSquare("b1", 0, "empty", ["a1", "a2", "b2", "c1", "c2"]);
+const b2 = new GridSquare("b2", 0, "empty", ["a1", "a2", "a3", "b1", "b3", "c1", "c2", "c3"]);
+const b3 = new GridSquare("b3", 0, "empty", ["a2", "a3", "a4", "b2", "b4", "c2", "c3", "c4"]);
+const b4 = new GridSquare("b4", 0, "empty", ["a3", "a4", "a5", "b3", "b5", "c3", "c4", "c5"]);
+const b5 = new GridSquare("b5", 0, "empty", ["a4", "a5", "b4", "c4", "c5"]);
+const c1 = new GridSquare("c1", 0, "empty", ["b1", "b2", "c2", "d1", "d2"]);
+const c2 = new GridSquare("c2", 0, "empty", ["b1", "b2", "b3", "c1", "c3", "d1", "d2", "d3"]);
+const c3 = new GridSquare("c3", 0, "empty", ["b2", "b3", "b4", "c2", "c4", "d2", "d3", "d4"]);
+const c4 = new GridSquare("c4", 0, "empty", ["b3", "b4", "b5", "c3", "c5", "d3", "d4", "d5"]);
+const c5 = new GridSquare("c5", 0, "empty", ["b4", "b5", "c4", "d4", "d5"]);
+const d1 = new GridSquare("d1", 0, "empty", ["c1", "c2", "d2", "e1", "e2"]);
+const d2 = new GridSquare("d2", 0, "empty", ["c1", "c2", "c3", "d1", "d3", "e1", "e2", "e3"]);
+const d3 = new GridSquare("d3", 0, "empty", ["c2", "c3", "c4", "d2", "d4", "e2", "e3", "e4"]);
+const d4 = new GridSquare("d4", 0, "empty", ["c3", "c4", "c5", "d3", "d5", "e3", "e4", "e5"]);
+const d5 = new GridSquare("d5", 0, "empty", ["c4", "c5", "d4", "e4", "e5"]);
+const e1 = new GridSquare("e1", 0, "empty", ["d1", "d2", "e2"]);
+const e2 = new GridSquare("e2", 0, "empty", ["d1", "d2", "d3", "e1", "e3"]);
+const e3 = new GridSquare("e3", 0, "empty", ["d2", "d3", "d4", "e2", "e4"]);
+const e4 = new GridSquare("e4", 0, "empty", ["d3", "d4", "d5", "e3", "e5"]);
+const e5 = new GridSquare("e5", 0, "empty", ["d4", "d5", "e4"]);
 
 
+
+//funcs to increase height of squares
 
 const printHeight = (gridSquare) => {
-   const theGridSquare = document.getElementById(gridSquare);
+   const theGridSquare = document.getElementById(gridSquare.position);
    theGridSquare.innerHTML = gridSquare.height;
 }
 
@@ -49,7 +52,8 @@ const printHeight = (gridSquare) => {
 
 const buildOnSquare = (gridSquare) => {
    console.log(gridSquare);
-   const theGridSquare = document.getElementById(gridSquare);
+   const theGridSquare = document.getElementById(gridSquare.position);
+   console.log(theGridSquare)
    theGridSquare.classList.remove("height-" + gridSquare.height);
    gridSquare.increaseHeight();
    theGridSquare.classList.add("height-" + gridSquare.height);
@@ -57,7 +61,6 @@ const buildOnSquare = (gridSquare) => {
 }
 
 const testyMcTestFace = (id) => {
-   // const theGridID = document.getElementsByClassName("grid-square")[0].id; //this line looks like current issue
    const theGridID = id;
    buildOnSquare(theGridID);
 }
